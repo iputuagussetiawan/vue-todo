a68f04c17d3e64ea5499406d832d4b9665e4dd85
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-04-29 09:53:27 +0800

    first setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import TaskPage from './pages/TasksPage.vue';
+  import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <TaskPage/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
