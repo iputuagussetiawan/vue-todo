80a60476c2009320053f401d65b0f1f1684b272c
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-04-29 16:54:19 +0800

    Programmatic navigations

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>Login</h1>
+    <button class="btn btn-primary" @click="handleClick"> Tasks</button>
 </template>
+
+<script setup>
+    import {useRouter} from "vue-router";
+    const router=useRouter()
+    const handleClick=()=>{
+        router.push({name:'tasks'})
+    }
+</script>
